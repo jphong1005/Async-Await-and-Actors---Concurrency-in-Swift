7a8330fed9a2af42475009dda8c70d701248ae14
Jinpyo Hong <jphongs1005@gmail.com>
2026-03-14 20:57:20 +0900

    [Feat] Understanding Structured Concurrency in Swift 프로젝트
    - Cancelling a Task

--- Concurrency-Tasks.playground/Contents.swift
+++ Concurrency-Tasks.playground/Contents.swift
@@ -3,6 +3,7 @@ import UIKit
 enum NetworkError: Error {
     case badUrl
     case decodingError
+    case invalidId
 }
 
 struct CreditScore: Decodable {
@@ -35,26 +36,14 @@ func getAPR(userId: Int) async throws -> Double {
         throw NetworkError.badUrl
     }
     
-    //  서로 다른 API 호출로, 각 await에서 Suspend 되고 Serial하게 실행 (=> 총 소요 시간이 큼)
-    //  await를 연속으로 사용해야 하는 경우
-    //      - 작업들이 서로 의존관계 (인과관계)를 갖는 경우
-    //      - 실행 순서가 보장되어야 하는 경우
-    /*
-    let (experianData, _): (Data, URLResponse) = try await URLSession.shared.data(from: experianUrl)
-    let (equifaxData, _): (Data, URLResponse) = try await URLSession.shared.data(from: equifaxUrl)
-     */
+    //  For testing the cancellation of the task
+    if (userId % 2 == 0) {
+        throw NetworkError.invalidId
+    }
     
-    //  async-let으로 2개의 API를 병렬 실행
-    //  async-let을 만나는 순간, 2개의 실행 흐름이 생김 (-> Parent Task & Child Task)
-    //      - Child Task: expression을 수행
-    //      - Parent Task: Child Task가 수행하는 결과값을 담을 placeholder를 생성하여 즉시 Binding
-    //                      + Child Task가 작업하는 동안 이후 statements 수행
     async let (experianData, _): (Data, URLResponse) = URLSession.shared.data(from: experianUrl)
     async let (equifaxData, _): (Data, URLResponse) = URLSession.shared.data(from: equifaxUrl)
     
-    //  실제 값이 필요한 시점에 await로 호출
-    //      - Child Task 완료 시: 기다림 없이 즉시 값을 가져옴
-    //      - Child Task 아직 실행 시: Parent Task는 suspend되어 Child Task가 끝날 때까지 기다림
     let experianCreditScore: CreditScore? = try? JSONDecoder().decode(CreditScore.self, from: try await experianData)
     let equifaxCreditScore: CreditScore? = try? JSONDecoder().decode(CreditScore.self, from: try await equifaxData)
     
@@ -66,23 +55,24 @@ func getAPR(userId: Int) async throws -> Double {
     return calculateAPR(creditScores: [experianCreditScore, equifaxCreditScore])
 }
 
-//  Async-let Tasks
-//      Task: 비동기 작업의 단위 (-> 독립실행 + 순차실행)
-Task {
-    let apr: Double = try await getAPR(userId: 1)
-    print("APR: \(apr)")
-}
-
-//  Async-let in a Loop
 let ids: [Int] = Array(1...5)
+var invalidIds: [Int] = []
 
 Task {
     for id in ids {
-        let apr: Double = try await getAPR(userId: id)
-        print("id: \(id) / apr: \(apr)")
-        
-        //  문제점
-        //      1. for-in Loop 안에서 각 id에 대해 순차실행 함 (Serial Execution)
-        //      2. getAPR() 내부에서 try await 도중 에러 발생 시, Task가 중단됨 (Child Task가 모두 완료되어야 Parent Task도 완료됨)
+        do {
+            //  Cancellation 2가지 방법
+            //      - Task.isCancelled: 취소 상태 확인만 함 (-> 리소스 정리 후 종료하거나 값을 반환할 때 사용)
+            //      - Task.checkCancellation(): 취소 여부 확인 후, 취소된 상태라면 즉시 CancellationError를 던짐 (-> 즉시 중단하고 에러처리를 맡길 때 사용)
+            
+            try Task.checkCancellation()
+            let apr: Double = try await getAPR(userId: id)
+            print("id: \(id) / apr: \(apr)")
+        } catch {
+            print("error: \(error)")
+            invalidIds.append(id)
+        }
     }
+    
+    print("\ninvalidIds: \(invalidIds)")
 }
